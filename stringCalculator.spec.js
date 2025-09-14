420ac0223abc14d4071f97c88593efe4184fcc8a
Rishabh <43580878+rishabh-in@users.noreply.github.com>
2025-09-14 11:12:56 +0530

    test(string-calculator): added test for multiple numbers (should pass)

--- stringCalculator.spec.js
+++ stringCalculator.spec.js
@@ -11,3 +11,7 @@ test('returns number itself when single number is passed', () => {
 test('returns sum of two numbers separated by comma', () => {
   expect(add("1,5")).toBe(6);
 });
+
+test('returns sum of multiple comma-separated numbers', () => {
+  expect(add("1,2,3,4")).toBe(10);
+});
