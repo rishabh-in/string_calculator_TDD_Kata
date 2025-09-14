5ac17782d1b766cf1ed3b3810041c548e5cfd538
Rishabh <43580878+rishabh-in@users.noreply.github.com>
2025-09-14 10:26:58 +0530

    test(string-calculator): added failing test for single number (RED)

--- stringCalculator.spec.js
+++ stringCalculator.spec.js
@@ -3,3 +3,7 @@ import add from './stringCalculator';
 test('returns 0 for empty string', () => {
   expect(add("")).toBe(0);
 });
+
+test('returns number itself when single number is passed', () => {
+  expect(add("1")).toBe(1);
+});
