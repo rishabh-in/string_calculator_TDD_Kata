f014201c2c4c342f9fa4e02e55e66f1b31e1381f
Rishabh <43580878+rishabh-in@users.noreply.github.com>
2025-09-14 11:16:13 +0530

    test(string-calculator): added failing test for custom delimiter (RED)

--- stringCalculator.spec.js
+++ stringCalculator.spec.js
@@ -19,3 +19,7 @@ test('returns sum of multiple comma-separated numbers', () => {
 test('handles new lines between numbers', () => {
   expect(add("1\n2,3")).toBe(6);
 });
+
+test('supports custom single-character delimiter with //;\\n', () => {
+  expect(add("//;\\n1;2")).toBe(3);
+});
