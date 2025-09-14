b879feee7059edd88b6801ee7de065ebb8569bbe
Rishabh <43580878+rishabh-in@users.noreply.github.com>
2025-09-14 10:29:20 +0530

    feat(string-calculator): implement single number handling (GREEN)

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,4 +1,8 @@
 export default function add(numbers) {
   if (numbers === "") return 0;
+  // if single number
+  if (!numbers.includes(',' ) && !numbers.includes('\n')) {
+    return Number(numbers);
+  }
   throw new Error('Not implemented');
 }
