2e68fea5cf24b9a26fca5fe3286ad8cc1895bc52
Rishabh <43580878+rishabh-in@users.noreply.github.com>
2025-09-14 11:11:19 +0530

    feat(string-calculator): support comma-separated numbers (GREEN)

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,8 +1,12 @@
 export default function add(numbers) {
   if (numbers === "") return 0;
+
   // if single number
-  if (!numbers.includes(',' ) && !numbers.includes('\n')) {
+  if (!numbers.includes(',') && !numbers.includes('\n')) {
     return Number(numbers);
   }
-  throw new Error('Not implemented');
+
+  // handle comma-separated numbers
+  const nums = numbers.split(',').map(n => Number(n));
+  return nums.reduce((a, b) => a + b, 0);
 }
